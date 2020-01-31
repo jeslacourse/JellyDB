525edc0e85a33e12368f8e2bf63e5ec2bb730c61
Ben Rausch <slimeguy46@gmail.com>
2020-01-31 10:43:32 -0800

    Move methods into table; fix Config Singleton bug

--- JellyDB/query.py
+++ JellyDB/query.py
@@ -2,19 +2,9 @@ from JellyDB.table import Table
 from JellyDB.index import Index
 
 
-# How to implement this class: we can, within this class, directly access the members and functions of table.
-# We could use those to, say, allocate new pages for insertion. However, that means Query is responsible for
-# Table's storage, and you couldn't have multiple query objects on the same table. I don't like that idea.
-# The other extreme: get rid of the Query class; Table owns all of the query-like methods.
-#
-# A "middle-ground" is that Table controls all of its page allocations, so that Query has no idea new pages
-# are being allocated for its inserts.
-#
-# I expect any approach to need revamping when we implement transactions.
-#
-# Also, it's a mystery where indices fit in - that's something we'll need to Google. They could be part of Query objects,
-# but should PROBABLY be members of the table itself so that any Query object can access them.
-# If we have indices on non-key attributes, I think that they will be costly to keep up-to-date.
+# The Table class performs all query behavior; this is just an envelope. This
+# class exists because the Professor's test script expects a class named
+# "Query" with these methods.
 
 class Query:
     """
@@ -22,49 +12,33 @@ class Query:
     """
     def __init__(self, table: Table):
         self.table: Table = table
-        pass
 
     """
-    # internal Method
-    # Read a record with specified RID
-    """
-    def _read(self, rid: int):
-        pass
-
-    """
-    # delete the record in self.table which has the value `key` in the column used for its primary key
-    :param key: int # the primary key value of the record we are deleting
+    # See table.py.
     """
     def delete(self, key: int):
-        pass
+        self.table.delete(key)
 
     """
-    # Insert a record with specified columns
-    :param columns: tuple   # expect a tuple containing the values to put in each column: e.g. (1, 50, 3000, None, 300000)
+    # The * combines multiple arguments to the function into one tuple, columns.
     """
     def insert(self, *columns):
-        schema_encoding = '0' * self.table.num_columns
-        pass
+        self.table.insert(columns)
 
     """
-    # Read a record with specified key
-    :param query_columns: list # Expect a list of integers: one per column. There will be a 1 if we are to read the column, a 0 otherwise.
+    # See table.py.
     """
     def select(self, key: int, query_columns):
-        pass
+        return self.table.select(key, query_columns)
 
     """
-    # Update a record with specified key and columns
-    # "takes as input a list of values for ALL columns of the table. The columns that are not being updated should be passed as None." - Parsoa
-    :param columns: tuple   # expect a tuple containing the values to put in each column: e.g. (1, 50, 3000, None, 300000)
+    # The * combines all arguments to the function after `key` into one tuple, columns.
     """
     def update(self, key: int, *columns):
-        pass
+        self.table.update(key, columns)
 
     """
-    :param start_range: int         # Start of the key range to aggregate
-    :param end_range: int           # End of the key range to aggregate
-    :param aggregate_columns: int   # Index of desired column to aggregate
+    # See table.py.
     """
     def sum(self, start_range: int, end_range: int, aggregate_column_index: int):
-        pass
+        return self.table.sum(start_range, end_range, aggregate_column_index)
--- JellyDB/table.py
+++ JellyDB/table.py
@@ -3,9 +3,7 @@ from JellyDB.page import *
 from time import time
 
 INDIRECTION_COLUMN = 0
-RID_COLUMN = 1
-TIMESTAMP_COLUMN = 2
-SCHEMA_ENCODING_COLUMN = 3
+TIMESTAMP_COLUMN = 1
 
 # Why not have a "PageRange" class? Because a "PageRange" is just data with no
 # functionality. We can use a list to represent it, and just use functions in
@@ -18,32 +16,69 @@ class Table:
     :param key: int             #Index of table key in columns
     """
     def __init__(self, name: str, num_columns, key: int, RID_allocator: RIDAllocator):
-        self.name = name
-        self.key = key
-        self.num_columns = num_columns
+        self._name = name
+        self._key = key
+        self._num_columns = num_columns
         self._RID_allocator = RID_allocator
-        self.page_directory = {} # only one copy of each key can be present in a page directory! i.e. records can't have the same key!
-        self.page_ranges = [] # list of lists of pages.
-        self.add_page_range()
+        self._page_directory = {} # only one copy of each key can be present in a page directory! i.e. records can't have the same key!
+        self._page_ranges = [] # list of lists of pages.
+        self._add_page_range()
 
-        self.recreate_page_directory()
+        self._recreate_page_directory()
+        pass
+
+    """
+    # delete the record in self.table which has the value `key` in the column used for its primary key
+    :param key: int # the primary key value of the record we are deleting
+    """
+    def delete(self, key: int):
+        pass
+
+    """
+    # Insert a record with specified columns
+    :param columns: tuple   # expect a tuple containing the values to put in each column: e.g. (1, 50, 3000, None, 300000)
+    """
+    def insert(self, columns: tuple):
+        pass
+
+    """
+    # Read a record with specified key
+    :param query_columns: list # Expect a list of integers: one per column. There will be a 1 if we are to read the column, a 0 otherwise.
+    """
+    def select(self, key: int, query_columns):
+        pass
+
+    """
+    # Update a record with specified key and columns
+    # "takes as input a list of values for ALL columns of the table. The columns that are not being updated should be passed as None." - Parsoa
+    :param columns: tuple   # expect a tuple containing the values to put in each column: e.g. (1, 50, 3000, None, 300000)
+    """
+    def update(self, key: int, columns: tuple):
+        pass
+
+    """
+    :param start_range: int         # Start of the key range to aggregate
+    :param end_range: int           # End of the key range to aggregate
+    :param aggregate_columns: int   # Index of desired column to aggregate
+    """
+    def sum(self, start_range: int, end_range: int, aggregate_column_index: int):
         pass
 
     def __merge(self):
         pass
     
-    def add_page_range(self):
-        # TODO request base and tail RIDs from self.db and generate storage for all base pages and one tail page
+    def _add_page_range(self):
+        # TODO request base and tail RIDs from self._RID_allocator and generate storage for all base pages and one tail page
         pass
 
     """
     :param page_range: int  # index of page range to add the tail page to
     """
-    def add_tail_page(self, page_range: int):
+    def _add_tail_page(self, page_range: int):
         # TODO get tail RIDs, generate the storage, add it to self.page_ranges[page_range]
-        self.recreate_page_directory()
+        self._recreate_page_directory()
     
-    def recreate_page_directory(self):
+    def _recreate_page_directory(self):
         # search through the pages in the page directory, check the base and
         # bound RIDs in each LogicalPage, and put them in the page directories.
         # example: RID 1111101 corresponds to the tenth row of the first tail
